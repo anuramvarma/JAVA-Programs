171dafb3b30e16fdb8e35d557367fa542ee2b5a0
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-09 18:23:49 +0530

    Create For_each_Loop.java

---
+++ Basics_of_Java/Loop/For_each_Loop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class For_each_Loop
+{
+public static void main(String args[])
+{
+  int a[]={1,2,3,4,5};
+  int sum=0;
+  for(int i:a)
+  {
+    sum+=i;
+    System.out.println(i);
+  }
+  System.out.println("total:"+sum);
+  }
+  }
