6b650ddcd6ac2f7fcc789bcf046f85db8e0d2d0a
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-09 18:13:33 +0530

    Create Character.java

---
+++ Basics_of_Java/Loop/Character.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Character
+{
+	public static void main(String args[])
+ {
+	char i;
+	System.out.println("Alphabets from A to Z=");
+	for(i='A';i<='Z';i++)
+  {
+   	System.out.print(i+" ");
+	}
+ }
+}
+	
