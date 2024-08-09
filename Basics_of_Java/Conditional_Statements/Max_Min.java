f3ed9feeb9c13d0a9c1cf93817ccd785b015744c
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-08-09 18:32:36 +0530

    Create Max_Min.java

---
+++ Basics_of_Java/Conditional_Statements/Max_Min.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Max_Min 
+{
+    public static void main(String args[]) 
+{
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter a two numbers to check max and min among them:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a>b) 
+          {
+            System.out.println(a+" is max");
+            System.out.println(b+" is min");
+          }
+        else 
+          {
+            System.out.println(b+" is max");
+            System.out.println(a+" is min");
+          }
+
+    }
+}
