dba3dfbcf2f4a5b4ea160666ccc70f3ace6cb315
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-09-19 10:24:56 +0530

    Update Inheritance.java

--- Excercise-4/Inheritance.java
+++ Excercise-4/Inheritance.java
@@ -2,20 +2,20 @@ class A
 {
 public void display() 
   {
-	System.out.println("Parent class");
-	}
+    System.out.println("Parent class");
+  }
 }
 class Inheritance extends A
 {
 public void run() 
   {
-	System.out.println("child class");
-	}
+    System.out.println("Child class");
+  }
 public static void main(String []args)
   {
-	Inheritance obj = new Inheritance();
-	obj.display();
-	obj.run();
-}
+    Inheritance obj = new Inheritance();
+    obj.display();
+    obj.run();
+   }
 }
 	
