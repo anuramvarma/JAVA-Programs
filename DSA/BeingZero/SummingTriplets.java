b972d7e79b2ec2dfe805c46274d3ea030423729f
Anuram Prakash Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2025-11-12 05:50:18 +0530

    Add SummingTriplets.java to calculate sum of three numbers

---
+++ DSA/BeingZero/SummingTriplets.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long A=sc.nextLong();
+        long B=sc.nextLong();
+        long C=sc.nextLong();
+
+        long Sum=A+B+C;
+        System.out.println(Sum);
+    }
+}
